79d102d729c4ff9e7bb0bacc07e588984c049b4e
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-03-14 14:18:47 +0000

    solve

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import Greeting from "./greeting.js";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Greeting/>
     </div>
   )
 }
---
+++ src/components/greeting.js
@@ -0,0 +1,40 @@
+import { Component } from "react";
+// import Counter  from "./counter";
+
+
+
+class Greeting extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = { value: ''}
+  
+    }
+
+     changedText = (e) => {
+        
+        const newValue = e.target.value;
+        debugger
+        this.setState({value : newValue});
+    }
+
+
+    render() {
+        return (
+            <div>
+                <label htmlFor="input">Enter your name:</label>
+                <br/> <br/>
+                <input 
+                type="text"
+                id = "input"
+                value={this.state.value}
+                onChange={this.changedText}
+                />
+                <p>{this.state.value ? "Hello " + this.state.value + "!" : null}</p>
+            </div>
+        );
+    }
+}
+
+export default Greeting;
